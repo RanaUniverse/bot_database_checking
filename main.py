2dec7277eb5a78b6caf418dd7d64c55af66da5e7
RanaUniverse <RanaUniverse321@gmail.com>
2025-03-22 14:07:31 +0530

    little edit with edit and delete message

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-import os
-
-
 import logging
+import os
+import random
+import asyncio
 
 from dotenv import load_dotenv
 
 from telegram import Update
+from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 from telegram import LinkPreviewOptions
 
 from telegram.ext import (
@@ -13,15 +14,13 @@ from telegram.ext import (
     ContextTypes,
     MessageHandler,
     CommandHandler,
+    CallbackQueryHandler,
     filters,
 )
 
 from telegram.constants import ParseMode
 
-from my_modules.some_url_link import (
-    ImageLinks,
-    MessageEffectEmojies,
-)
+from my_modules.some_url_link import ImageLinks, MessageEffectEmojies
 
 
 logging.basicConfig(
@@ -33,8 +32,14 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
+GOOD_EFFECTS = [
+    MessageEffectEmojies.LIKE.value,
+    MessageEffectEmojies.HEART.value,
+    MessageEffectEmojies.TADA.value,
+]
 
-async def start_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+
+async def start_cmd_old(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """It will exxecute when /start will come"""
 
     if update.message is None or update.message.from_user is None:
@@ -45,7 +50,7 @@ async def start_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     text = (
         f"Hello {user.full_name} You have just start this bot thanks. "
-        f"The link is: Nothing"
+        f"I will send you this message, and after 3 second i will edit this again."
     )
 
     link_previes_options = LinkPreviewOptions(
@@ -55,12 +60,58 @@ async def start_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         prefer_small_media=True,
     )
 
+    msg_send = await context.bot.send_message(
+        chat_id=user.id,
+        text=text,
+        parse_mode=ParseMode.HTML,
+        link_preview_options=link_previes_options,
+        message_effect_id=random.choice(GOOD_EFFECTS),
+    )
+
+    await asyncio.sleep(3)
+    await context.bot.edit_message_text(
+        chat_id=user.id,
+        text="This is new message content.",
+        message_id=msg_send.message_id,
+        parse_mode=ParseMode.HTML,
+    )
+
+
+async def start_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """It will exxecute when /start will come"""
+
+    if update.message is None or update.message.from_user is None:
+        print("This should Not happens 🧐🧐🧐")
+        return
+
+    link_previes_options = LinkPreviewOptions(
+        is_disabled=True,
+        url=ImageLinks.RU_IMAGE.value,
+        show_above_text=True,
+        prefer_small_media=True,
+    )
+    keyboard = [
+        [
+            InlineKeyboardButton(text="Option 1", callback_data="1"),
+            InlineKeyboardButton(text="Option 2", callback_data="2"),
+        ],
+        [
+            InlineKeyboardButton(text="Option 3", callback_data="3"),
+            InlineKeyboardButton(text="Option 4", callback_data="4"),
+            InlineKeyboardButton(text="Option 5", callback_data="5"),
+        ],
+    ]
+    user = update.message.from_user
+
+    text = f"Hello {user.full_name} This is a checking button message."
+
     await context.bot.send_message(
         chat_id=user.id,
         text=text,
         parse_mode=ParseMode.HTML,
         link_preview_options=link_previes_options,
-        message_effect_id=MessageEffectEmojies.HEART.value,
+        message_effect_id=random.choice(GOOD_EFFECTS),
+        reply_markup=InlineKeyboardMarkup(keyboard),
     )
 
 
@@ -82,6 +133,46 @@ async def echo_msg(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await context.bot.send_message(user.id, text, ParseMode.HTML)
 
 
+async def button(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """
+    wher user will press button it should be execute with proper callback data
+    """
+
+    if update.callback_query is None:
+        print("This shuld not happens as always callback query exists.")
+        return
+
+    query = update.callback_query
+
+    await context.bot.answerCallbackQuery(
+        callback_query_id=query.id,
+        text=f"You have pressed this button.",
+        show_alert=True,
+    )
+
+    user = query.from_user
+    button_data = query.data
+
+    text = (
+        f"Hello {user.full_name} The button data which was assign with the button is \n"
+        f"<blockquote>{button_data}</blockquote> \n"
+        f"The Button Message will delete after 3 Seconds."
+    )
+
+    msg_send = await context.bot.send_message(  # type: ignore
+        chat_id=user.id,
+        text=text,
+        parse_mode=ParseMode.HTML,
+    )
+
+    await asyncio.sleep(3)
+
+    await context.bot.deleteMessage(
+        chat_id=user.id,
+        message_id=query.message.message_id,  # type: ignore
+    )
+
+
 def main() -> None:
     """STarting the bot"""
 
@@ -99,6 +190,13 @@ def main() -> None:
 
     application.add_handler(
         CommandHandler(
+            command=["help"],
+            callback=start_cmd_old,
+            block=False,
+        )
+    )
+    application.add_handler(
+        CommandHandler(
             command=["start", "st"],
             callback=start_cmd,
             block=False,
@@ -109,6 +207,13 @@ def main() -> None:
         MessageHandler(
             filters=filters.TEXT,
             callback=echo_msg,
+            block=False,
+        )
+    )
+
+    application.add_handler(
+        CallbackQueryHandler(
+            callback=button,
             block=False,
         )
     )
